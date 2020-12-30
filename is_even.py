c7377dafb47e5430cc6976e860c220035d20f951
lenahong1 <76773479+lenahong1@users.noreply.github.com>
2020-12-30 11:33:55 +0100

    Update is_even.py

--- is_even.py
+++ is_even.py
@@ -1,12 +1,1 @@
-number = input("Input your number: ")
-
-try:
-    number = int(number)
-except ValueError:
-    print("This is not a number, exiting...")
-    exit()
-
-if number % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
+git commit --allow-empty -m "Create a CodeGrade submission" && git push
